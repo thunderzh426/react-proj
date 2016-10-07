e3810699eba5269b71f0abe8240df4af915b28f0
Lei Zhang <zhang.le@husky.neu.edu>
2016-10-07 16:30:47 -0400

    init

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>OSC Administration</h1>
+        <p>OSC is a consulting company!!</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;
